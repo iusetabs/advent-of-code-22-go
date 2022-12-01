417d0ab342721f05c6413e9b46858a4a16bba242
James Nolan <james.nolan38@mail.dcu.ie>
2022-12-01 21:19:11 +0000

    Day1 Part1

---
+++ day1/day1.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"log"
+	"strings"
+	"strconv"
+)
+
+func scanUntilEmptyLine(scanner *bufio.Scanner, returnList []string) []string {
+	scanner.Scan()
+	nextLine := strings.TrimSpace(scanner.Text())
+	log.Println(fmt.Sprintf("%s%s", "nextLine: ", nextLine))
+	if len(nextLine) == 0 {
+		return returnList
+	} 
+	return scanUntilEmptyLine(scanner, append(returnList, nextLine))
+}
+
+func countCalories(lines []string, returnNum int) int {
+	if len(lines) == 0 {
+		return returnNum
+	}
+	nextCalorie, err := strconv.Atoi(lines[0])
+	if err != nil  {
+		panic(fmt.Sprintf("%s%v%s", "Could not convert non string: ", lines[0], " to int"))
+	}
+
+	return countCalories(lines[1:], returnNum+nextCalorie)
+}
+
+func getIndividualElfCalories(scanner *bufio.Scanner, returnList []int, i int) []int {
+	log.Println(fmt.Sprintf("%s%d", "Reading elf #", i))
+	lines := scanUntilEmptyLine(scanner, []string{})
+	log.Println(fmt.Sprintf("%s%d%s%v", "elf #", i, " :", lines))
+	if len(lines) == 0 {
+		return returnList
+	} 
+	totalCalories := countCalories(lines, 0)
+	return getIndividualElfCalories(scanner, append(returnList, totalCalories), i+1)
+}
+
+func max(array []int, currentMax int) int {
+	if len(array) == 0 {
+		return currentMax
+	}
+	updatedMax := currentMax
+	if array[0] > currentMax {
+		updatedMax = array[0]
+	}
+
+	return max(array[1:], updatedMax)
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	elfCalorieCounts := getIndividualElfCalories(scanner, []int{}, 1)
+	log.Println(elfCalorieCounts)
+	log.Println(fmt.Sprintf("%s%d", "Max calorie: ", max(elfCalorieCounts, 0)))
+}
